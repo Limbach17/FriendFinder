dfe31a876d4dc9c56ffffeb9bf808d2aba37d769
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-03 00:24:59 -0400

    friends.js update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 var express = require("express");
 var router = express.Router();
 
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 
 router.get("/api/friends", function(req, res){
     res.json(friendsData);
