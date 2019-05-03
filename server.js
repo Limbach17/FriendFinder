ea8467f10ddce53dbae0fbd187f37fa6df9b62ce
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-03 00:21:27 -0400

    handle-bars requirement added.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var path = require("path");
 var express = require("express");
+var exphbs = require("express-handlebars");
+
 var env = require("dotenv").config();
 
 var api = require("./app/routing/apiRoutes.js");
