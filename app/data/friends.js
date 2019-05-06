c25b6102b993b74d594bc45f92c81e6d3a068c74
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-06 07:33:54 -0400

    updated with math and modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
     {
-    "name":"Doggo",
-    "photo":"https://www.google.com/search?q=doggo&rlz=1C1CHBF_enUS841US841&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjJyZjPmv7hAhVlhuAKHY9qD_MQ_AUIDigB&biw=2560&bih=1298#imgrc=t8xxNVFThAUSrM:",
-    "scores":[
+    name:"Doggo",
+    picture:"https://www.google.com/search?q=doggo&rlz=1C1CHBF_enUS841US841&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjJyZjPmv7hAhVlhuAKHY9qD_MQ_AUIDigB&biw=2560&bih=1298#imgrc=t8xxNVFThAUSrM:",
+    scores:[
         5,
         1,
         4,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,23 +9,46 @@ router.get("/api/friends", function(req, res){
 });
 
 router.post("/api/friends", function(req,res){
-   /*  console.log(req.body);
-    var currentUser = req.body.newUser;
+    console.log(req.body);
+    var currentUser = req.body;
+    var userScores = currentUser.scores;
     
-    var bestMatches = [];
+    var bestMatch = {
+        name: "",
+        picture: "",
+        scoreDiff: 0
+
+    };
+
+    var totalDiff;
 
     for (var i = 0; i < friendsData.length; i++){
-        var comparison = friendsData.length[i];
-        var comparisonDiff;
+        comparison = friendsData[i];
+        console.log(comparison.name);
+        totalDiff = 0;
         
-        for (var j = 0; j < 10; j++){
-            comparisonDiff += Math.abs(comparison.scores[j] - currentUser.scores[j]);
+        for (var j = 0; j < comparison.scores.length; j++){
+             var comparisonScore = comparison.scores[j];
+             var currentUserScore = userScores[j];
+
+             totalDiff += Math.abs(parseInt(currentUser) - parseInt(comparisonScore));
         }
 
-    }; */
+        if (totalDiff <= bestMatch.scoreDiff) {
+            bestMatch.name = comparison.name;
+            bestMatch.picture = comparison.picture;
+            bestMatch.scoreDiff = totalDiff;
+        }
+
+        console.log(bestMatch.name + "\n");
+        console.log(bestMatch.picture + "\n");
+        console.log(bestMatch.scoreDiff + "\n");
+
+    };
 
     friendsData.push(req.body);
-    alert("Holy friends, Batman!");
+    res.json(bestMatch);
+    
 });
 
 module.exports = router;
